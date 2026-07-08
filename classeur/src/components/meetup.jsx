c455cbdb02575b4965613bf8f0318d97e083fad6
zen` <wintertemper479@gmail.com>
2026-07-08 09:42:29 +0800

    hm

---
+++ classeur/src/components/meetup.jsx
@@ -0,0 +1,21 @@
+export default function Meetup() {
+
+    return (
+
+        <div className="w-full min-h-screen">
+
+            <iframe
+                src="/meetup.html"
+                width="100%"
+                height="900"
+                style={{
+                    border: "none",
+                    borderRadius: "12px"
+                }}
+            />
+
+        </div>
+
+    );
+
+}
